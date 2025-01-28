f8c3eef1c7efbed36ca3be4bf7024b1258c0a1a3
Tarek Laun <63686151+Tarek-Laun@users.noreply.github.com>
2025-01-28 09:11:06 +0100

    Add Engine Enums and Update Docs

--- engine/include/Engine.hpp
+++ engine/include/Engine.hpp
@@ -3,6 +3,13 @@
 
 namespace CanvasForge {
 
+  enum class RenderMode {
+    Windowed = 0,
+    FullScreen = 1,
+    Borderless = 2,
+    Custom = 3
+  };
+
   enum class RenderAPI {
     None = 0,
     Vulkan = 1,
@@ -10,6 +17,6 @@ namespace CanvasForge {
 
   class Engine {
   public:
-    static void Init();
+    static void Init(std::string _title, int _width, int _height, RenderMode _mode, RenderAPI _api);
   };
 }
