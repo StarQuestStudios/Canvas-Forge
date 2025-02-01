26c6fca71bb5a4f202696e10e74447a0c57c0bdc
Tarek-Laun <Tarek.Laun@lk-industries.com>
2025-02-01 15:50:59 +0100

    Add Utils

---
+++ engine/include/core/Utils.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "CFpch.hpp"
+
+namespace CanvasForge
+{
+  class Utils {
+  public:
+    
+  };
+}
--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -17,7 +17,7 @@ namespace CanvasForge {
 
 
       /* Setup Renderer */
-
+      
 
     }
     
